8657c0d666a80bd1cb24e7a0c840ccdbc2c15fba
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2020-06-25 21:04:38 +0300

    command line checker

---
+++ bash_gui/commandlineparametercheck.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$#" != "3" ]; then #number of parameter not equal to 3
+	echo "USAGE: commandlineparameter.sh [PARM1] [PARM2] [PARM3]"
+	exit 400
+fi
+
+echo "Exited!!"
---
+++ bash_gui/commandlineparametercheck_otherway.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+: ${3?"USAGE: commandlineparameter_otherway.sh [PARM1] $1 [PARM2] $2  [PARM3] $3"} #shortcut if 
+echo "Exited!! got all 3"
